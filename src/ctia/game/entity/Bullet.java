ef11f3025f3879bfcee0045c3340683883fb6959
Shiliang Zhang <lsftw13@gmail.com>
2014-11-05 18:19:50 -0500

    Added projectile firing.

---
+++ src/ctia/game/entity/Bullet.java
@@ -0,0 +1,14 @@
+package ctia.game.entity;
+
+import ctia.engine.core.Entity;
+import ctia.engine.entity.Projectile;
+
+public class Bullet extends Projectile {
+	private static final int SPEED = 30;
+	public Bullet(Entity owner, double angle) {
+		super(owner, owner.getPx(), owner.getPy());
+		damage = 20;
+		vx = Math.cos(angle) * SPEED;
+		vy = Math.sin(angle) * SPEED;
+	}
+}
