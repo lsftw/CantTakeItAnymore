dbb9e21b021d6e11768e874fe1b926b0539819bd
Shiliang Zhang <lsftw13@gmail.com>
2014-11-12 19:07:50 -0500

    Bullets now fire from center.

--- src/ctia/game/entity/projectile/PiercingBullet.java
+++ src/ctia/game/entity/projectile/PiercingBullet.java
@@ -7,6 +7,8 @@ public class PiercingBullet extends Projectile {
 	private static final int SPEED = 30;
 	public PiercingBullet(Entity owner, double angle) {
 		super(owner, owner.getPx(), owner.getPy());
+		px = px + owner.getSx() / 2 - sx / 2;
+		py = py + owner.getSy() / 2 - sy / 2;
 		damage = 20;
 		vx = Math.cos(angle) * SPEED;
 		vy = Math.sin(angle) * SPEED;
